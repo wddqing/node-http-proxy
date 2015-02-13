a0a34d17acad3d3494acbfa1090cfbd10bda4bed
wddqing <wddqing@gmail.com>
2015-02-13 00:53:38 -0800

    create dev master

--- bin/main.js
+++ bin/main.js
@@ -3,13 +3,18 @@
  * Created by wddqing on 15/2/3.
  */
 
-//var ProxyMaster = require('../lib/proxy-master')
+console.log(process.argv);
 
-//var proxymaster = new ProxyMaster()
+var ProxyMaster = require('../lib/proxy-master')
 
-//proxymaster.start();
+var proxymaster = new ProxyMaster()
+
+proxymaster.start();
+
+/*
 var http = require('http')
 http.createServer(function(req,res){
     res.end('hello')
 }).listen(8888)
 console.dir(process.argv)
+*/
